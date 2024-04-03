2632d6b4b59259d7e53c02a7a912e53897a411ab
kevin <1171407839@qq.com>
2024-04-03 10:42:14 +0000

    :sparkles: Support Coding, Complement and Encoding

--- src/components/ImageOutput.tsx
+++ src/components/ImageOutput.tsx
@@ -1,27 +1,10 @@
-import { Card, CardMedia, CardActions } from "@mui/material";
-
-import { useCodingStore } from "../store";
+import {useCodingStore} from "../store";
 
 function ImageOutput() {
-  const { encodedString } = useCodingStore();
-
+  const {encodedString} = useCodingStore();
+  
   return (
-    <Card>
-      <CardMedia
-        component="img"
-        height="100%"
-        image={`http://www.plantuml.com/plantuml/svg/${encodedString}`}
-      />
-      <CardActions>
-        <a
-          href={`http://www.plantuml.com/plantuml/uml/${encodedString}`}
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open in PlantUML
-        </a>
-      </CardActions>
-    </Card>
+    <img className="w-full" alt="output" src={`http://www.plantuml.com/plantuml/svg/${encodedString}`}/>
   );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react-swc";
-import mkcert from "vite-plugin-mkcert";
+import unocss from "@unocss/vite"
+
+import {vitePluginForArco} from '@arco-plugins/vite-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
-  server: {
-    https: true,
-  },
+  plugins: [unocss(), react(), vitePluginForArco({
+    style: 'css'
+  })],
 });
